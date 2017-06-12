44b88af004f0e970409866b8535e94ab65f38a2a
easy1090 <752355956@qq.com>
2017-06-12 22:30:45 +0800

    Create global.js

--- js/global.js
+++ js/global.js
@@ -213,6 +213,7 @@ $(document).on('mousewheel DOMMouseScroll', function(e){
 
     e.preventDefault();
 });
+$(docunment).on('touchstart touchmove',handleTouchEvent(e));
 $(document).ready(function(){
 	$('a[href="#"]').click(function (e) {
 		// body...
